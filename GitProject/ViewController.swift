2af2fcfbd5b6f37b0b7ca9353a7e76ce775a2aac
Sainath <sainathbamen3697@gmail.com>
2024-01-10 15:07:10 +0530

    Added new Gray color uiview.

--- GitProject/ViewController.swift
+++ GitProject/ViewController.swift
@@ -9,17 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newView: UIView!
+    
     @IBOutlet weak var btnAction: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        newView.backgroundColor = .darkGray// second change.
         btnAction.titleLabel?.text = "Show Modal"
         navigationController?.title = "Xcode and Git" // first change...
         btnAction.backgroundColor = UIColor .red
-
+        
     }
 
 
     @IBAction func btnAction(_ sender: UIButton) {
+
         
         
         
